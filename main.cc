1a713ec8ca0b712ed5f53420fc031d8749423888
wangziyu <wangziyuruc@gmail.com>
2018-03-22 03:55:48 -0700

    main

---
+++ main.cc
@@ -0,0 +1,27 @@
+#include "leveldb/db.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+using namespace leveldb;
+
+int main() {
+
+    leveldb::DB *db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "testdb", &db);
+    cout << status.ok() <<endl;
+    assert(status.ok());
+
+    status = db->Put(WriteOptions(), "JimZuoLin", "Hello Jim!");
+    assert(status.ok());
+    string res;
+    status = db->Get(ReadOptions(), "JimZuoLin", &res);
+    assert(status.ok());
+    cout << res << endl;
+    getchar();
+
+    delete db;
+    return 0;
+}
